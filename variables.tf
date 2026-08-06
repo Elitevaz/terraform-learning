e51b424c3f1c97408c47f4f5c42d64f35bd1cd9a
Elitevaz <vaz9999@yahoo.com>
2026-08-06 02:34:21 -0400

    added commit and ignore files

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "ubuntu_ip" {
+  type        = string
+  description = "The hidden IP address of the local Ubuntu server"
+}
+
+variable "ubuntu_user" {
+  type        = string
+  default     = "ubuntu"
+}
